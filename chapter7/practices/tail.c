66e1224a83c2b60268a08ea78f5572265132928e
tkdn <tkdnation@gmail.com>
2026-05-05 17:29:52 +0900

    feat: chap7 practices tail

---
+++ chapter7/practices/tail.c
@@ -0,0 +1,55 @@
+// https://github.com/aamine/stdlinux2-source/blob/master/tail.c
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N_LINES 10
+#define MAX_LINE_LENTGH 1024
+
+static void tail(FILE *f);
+
+int main(int argc, char *argv[])
+{
+  if (argc != 1)
+  {
+    fprintf(stderr, "Usage: %s < infle\n", argv[0]);
+    exit(1);
+  }
+  tail(stdin);
+  exit(0);
+}
+
+static char ring_buffer[N_LINES][MAX_LINE_LENTGH];
+
+#define RING_BUFFER_INC(idx) \
+  do                         \
+  {                          \
+    idx++;                   \
+    if (idx >= N_LINES)      \
+    {                        \
+      idx = 0;               \
+    }                        \
+  } while (0)
+
+static void
+tail(FILE *f)
+{
+  long curr = 0;
+  long n_read_lines = 0;
+
+  while (fgets(ring_buffer[curr], MAX_LINE_LENTGH, f))
+  {
+    RING_BUFFER_INC(curr);
+    n_read_lines++;
+  }
+  if (n_read_lines < N_LINES)
+  {
+    curr = 0;
+  }
+
+  int n;
+  for (n = N_LINES; n > 0 && n_read_lines; n--, n_read_lines--)
+  {
+    printf("%s", ring_buffer[curr]);
+    RING_BUFFER_INC(curr);
+  }
+}
