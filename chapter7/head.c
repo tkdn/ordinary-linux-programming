5fca74d87c3d18da24365815142c5efe6d566ff6
tkdn <tkdnation@gmail.com>
2026-05-05 13:32:37 +0900

    feat: chap7 head ver3

--- chapter7/head.c
+++ chapter7/head.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _GNU_SOURCE
+#include <getopt.h>
 
 static void do_head(FILE *f, long nlines);
 
+#define DEFAULT_N_LINES 10
+
+static struct option logopts[] = {
+    {"lines", required_argument, NULL, 'n'},
+    {"help", no_argument, NULL, 'h'},
+    {0, 0, 0, 0},
+};
+
 int main(int argc, char *argv[])
 {
-  long nlines;
+  int opt;
+  long nlines = DEFAULT_N_LINES;
 
-  if (argc < 2)
+  while ((opt = getopt_long(argc, argv, "n:", logopts, NULL)) != -1)
   {
-    fprintf(stderr, "Usage: %s n [file file...]\n", argv[0]);
-    exit(1);
+    switch (opt)
+    {
+    case 'n':
+      nlines = atol(optarg);
+      break;
+    case 'h':
+      fprintf(stdout, "Usage: %s [-n LINES] [FILE...]\n", argv[0]);
+      exit(0);
+    case '?':
+      fprintf(stderr, "Usage: %s [-n LINES] [FILE...]\n", argv[0]);
+      exit(1);
+    default:
+      break;
+    }
   }
-  nlines = atol(argv[1]);
-  if (argc == 2)
+
+  if (optind == argc)
   {
-    do_head(stdin, atol(argv[1]));
+    do_head(stdin, nlines);
   }
   else
   {
